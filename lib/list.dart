29dbf106862b1fd8c9098dcc881477d85e402ce4
eliasworku02 <eliasworku02@gmail.com>
2025-04-26 20:08:50 +0000

    change  list

--- lib/list.dart
+++ lib/list.dart
@@ -4,33 +4,58 @@ import 'appbar.dart';
 
 class Listmenu extends StatelessWidget {
   const Listmenu({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: CustomAppBar(),
       body: Stack(
   children: [
     Background(), // background at the bottom
-    Center(       // text at the center
-      child: Container(
-        child: Column(
-          children: [
-            Container(
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
+    Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Center(       // text at the center
+          child: Column(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: screenWidth*0.4,
+                width: screenWidth*0.6,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(14),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Calculate The Calendar",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),  
+                    ),
+                    SizedBox(
+                      child: ClipRRect(
+                        child: Image(image: 
+                                   AssetImage("aassets/Easter.png"),
+                                   //fit: BoxFit.cover
+                                   ),
+                      ),
+                    ),
+                  ],
+                ),
+      
               ),
-
-            ),
-          ],
+            ],
+          ),   
         ),
-           
-      ),
     ),
-
   ],
 ),
     );
